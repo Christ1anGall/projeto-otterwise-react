b1336b52dff4b2c43a2afb0fd392c6792e37c92c
Christian Gall <christiangall92@icloud.com>
2022-02-18 21:50:43 -0300

    criado lay do background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import Banner from "./components/Banner/Banner";
 import { DescribeCard } from "./components/DescribeCard";
-import todoIMG from "./components/img/todoIMG.svg"
-import './components/Banner/Banner.css'
-
-
+import todoIMG from "./components/img/todoIMG.svg";
+import "./components/Banner/Banner.css";
+import "./components/App.css";
 
 function App() {
-  
   return (
+    <div className="container">
+      <h1> Kanban</h1>
+      <div className="Kanban">
+        <Banner imgBanner={todoIMG} nameBanner="To Do" card="card">
+          <DescribeCard />
+        </Banner>
 
-    <div >
-      
-    <Banner imgBanner= {todoIMG} nameBanner="To Do" card="card" >
-     
-        <DescribeCard />
-        
-      </Banner>
-
+        <Banner nameBanner="To Do" />
       </div>
+    </div>
   );
 }
 
